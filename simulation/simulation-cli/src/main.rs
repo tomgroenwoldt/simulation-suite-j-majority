c6e1854277a636dfc68e4e996f9dccaebda97054
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2023-04-19 20:28:30 +0200

    Move config to own module

--- simulation/simulation-cli/src/main.rs
+++ simulation/simulation-cli/src/main.rs
@@ -1,29 +1,10 @@
 use clap::Parser;
+use config::Config;
 use simulation::Simulation;
 
 pub mod agent;
+pub mod config;
 pub mod simulation;
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-pub struct Config {
-    #[arg(short, long, value_parser = clap::value_parser!(u64).range(1..))]
-    agent_count: u64,
-
-    #[arg(short, long)]
-    sample_size: u8,
-
-    #[arg(short, long)]
-    opinion_count: u8,
-}
-
-impl Config {
-    pub fn validate(&self) {
-        if self.sample_size as u64 > self.agent_count {
-            panic!("It is not possible to sample a greater number of agents than the total number of agents currently present.");
-        }
-    }
-}
 
 #[derive(Debug, Default)]
 pub enum State {
