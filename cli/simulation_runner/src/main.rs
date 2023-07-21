da2b966c5e9d26ea29923ce46a374ebcf56fdeab
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2023-07-21 21:58:53 +0200

    Allow parallel simulations via simulation_runner

--- cli/simulation_runner/src/main.rs
+++ cli/simulation_runner/src/main.rs
@@ -1,43 +1,61 @@
-use args::Args;
-use simulation::Simulation;
 use std::fs::{create_dir_all, read_to_string, File};
 use std::io::Write;
 use std::path::Path;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 use anyhow::Result;
 use clap::Parser;
+
+use args::Args;
+use simulation::Simulation;
 
 mod args;
 
 fn main() -> Result<()> {
     // Parse CLI arguments and initialize logger
     let args = Args::parse();
-    let output_folder = args.output.clone();
     env_logger::Builder::new()
         .filter_level(args.verbose.log_level_filter())
         .init();
 
-    let mut simulations = vec![];
+    let simulations = Arc::new(Mutex::new(vec![]));
+    let mut handlers = vec![];
 
-    // Run single simulation
-    let simulation_config = args.info_simulation_config()?;
-    let mut simulation = Simulation::new(simulation_config)?;
-    simulation.execute();
-    simulations.push(simulation);
+    // Run simulations in parallel
+    let simulation_config = args.to_simulation_config()?;
+    let simulation = Simulation::new(simulation_config)?;
+    for _ in 0..args.simulations {
+        let mut simulation = simulation.clone();
+        let simulations = Arc::clone(&simulations);
+        let handler = thread::spawn(move || {
+            simulation.execute();
+            simulations.lock().unwrap().push(simulation);
+        });
+        handlers.push(handler);
+    }
 
-    // Create the file if it does not exist yet
-    let path = format!("output/{}/simulation.json", output_folder);
+    // Wait for all simulations to finish
+    handlers
+        .into_iter()
+        .for_each(|handler| handler.join().expect("Simulation thread panicked"));
+
+    let mut simulations = simulations.lock().unwrap();
+
+    let path = format!("output/{}/simulation.json", args.output);
+    // If a file already exists, read previous simulations
     if Path::new(&path).exists() {
         let mut previous_simulations: Vec<Simulation> =
             serde_json::from_str(&read_to_string(&path)?)?;
         simulations.append(&mut previous_simulations);
     } else {
-        let dir_path = format!("output/{}", output_folder);
+        let dir_path = format!("output/{}", args.output);
         create_dir_all(dir_path)?;
     };
-    let mut file = File::create(&path)?;
-    let export = serde_json::to_string_pretty(&simulations)?;
 
+    // Create a fresh file and store previous and new simulations in JSON format
+    let mut file = File::create(&path)?;
+    let export = serde_json::to_string_pretty(&simulations.clone())?;
     file.write_all(export.as_bytes())?;
 
     Ok(())
